a0d3261ae1082632302a900306c40d9dfa07ea68
Administrator <Administrator@zml>
2015-06-30 22:20:01 +0800

    放大缩小图片

---
+++ src/com/example/sample3_6/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sample3_6;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+	ImageView iv;
+	Bitmap bmp;
+	int screenWidth,screenHeight;
+	float scaleWidth,scaleHeight;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bmp=BitmapFactory.decodeResource(getResources(), R.drawable.xiaohei);
+		DisplayMetrics dm=new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		screenWidth=dm.widthPixels;
+		screenHeight=dm.heightPixels-80;
+		
+	}
+
+}
